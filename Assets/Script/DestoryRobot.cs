4c6be816dfef65fd66c72cd05c923868752e60f1
Chenggang Zhong <cgzhong17@gmail.com>
2026-03-28 19:59:47 -0400

    Destroy Robot
    Temp destroy robot script

---
+++ Assets/Script/DestoryRobot.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DestoryRobot : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Bullet"))
+            Destroy(gameObject);
+    }
+}
